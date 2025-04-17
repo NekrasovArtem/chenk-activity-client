37fdf4810e7407ee4e73c3eb609ab475e0eb1f18
NekrasovArtem <art3midus@yandex.com>
2025-04-17 23:14:55 +0500

    установил vuelidate

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,44 +6,72 @@ const router = createRouter({
 		{
 			path: '/auth',
 			name: 'Auth',
+			meta: {
+				title: 'Авторизация'
+			},
 			component: () => import('@/views/Auth/AuthView.vue'),
 		},
 		{
 			path: '/',
 			name: 'Home',
+			meta: {
+				title: 'Главная'
+			},
 			component: () => import('@/views/HomeView.vue'),
 		},
 		{
 			path: '/about',
 			name: 'About',
+			meta: {
+				title: 'О системе'
+			},
 			component: () => import('@/views/AboutView.vue'),
 		},
 		{
 			path: '/profile',
 			name: 'Profile',
+			meta: {
+				title: 'Профиль'
+			},
 			component: () => import('@/views/ProfileView.vue'),
 		},
 		{
 			path: '/groups',
 			name: 'Groups',
+			meta: {
+				title: 'Группы'
+			},
 			component: () => import('@/views/Groups/GroupsView.vue'),
 		},
 		{
 			path: '/groups/:id',
 			name: 'GroupDetail',
+			meta: {
+				title: 'Детальная группы'
+			},
 			component: () => import('@/views/Groups/GroupDetailView.vue'),
 		},
 		{
 			path: '/events',
 			name: 'Events',
+			meta: {
+				title: 'Мероприятия'
+			},
 			component: () => import('@/views/Events/EventsView.vue'),
 		},
 		{
 			path: '/events/:id',
 			name: 'EventDetail',
+			meta: {
+				title: 'Детальная мероприятия'
+			},
 			component: () => import('@/views/Events/EventDetailView.vue'),
 		},
 	],
+})
+
+router.beforeEach((to) => {
+	document.title = `${to.meta.title} | ЧЭнК Активность`;
 })
 
 export default router
